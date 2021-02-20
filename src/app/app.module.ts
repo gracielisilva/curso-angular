df77e665ce849d774fbe6a9737636408ed7edf2b
gcsilva <gcsilva@uoldiveo.com>
2021-02-20 14:59:05 -0300

    geat: Adicionando pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
+import { EerciciosDiretivasComponent } from './eercicios-diretivas/eercicios-diretivas.component';
+import { ExercicioContadorComponent } from './exercicio-contador/exercicio-contador.component';
+import { ExercicioDataDatabindingComponent } from './exercicio-data-databinding/exercicio-data-databinding.component';
+import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ExercicioDataDatabindingComponent } from './exercicio-data-databinding/exercicio-data-databinding.component';
-import { ExercicioContadorComponent } from './exercicio-contador/exercicio-contador.component';
-import { EerciciosDiretivasComponent } from './eercicios-diretivas/eercicios-diretivas.component';
-import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -21,12 +26,21 @@ import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass
     ExercicioContadorComponent,
     EerciciosDiretivasComponent,
     ExercicioNgclassComponent,
+    ExercicioPipesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt'
+  }],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function newFunction() {
+  registerLocaleData(localePt, 'pt');
+}
+
