ecc6b15a917b559a48f7723842da7fdc2a0511fd
gcsilva <gcsilva@uoldiveo.com>
2021-02-23 18:09:50 -0300

    feat: adicionado extrato service

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ExtratoService } from './extrato.service';
+
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
+  transacoes = [];
 
-  constructor() { }
+  constructor(
+    private extratoService: ExtratoService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.transacoes = this.extratoService.getTransacoes();
   }
-
 }
